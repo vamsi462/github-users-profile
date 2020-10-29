ed4b05f7875815e687fa1703b1448ba261ab1b71
vamsi krishna <vamsikrishnaadusumalli999@gmail.com>
2020-10-29 15:22:26 +0530

    file structure

---
+++ src/pages/Error.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from 'styled-components'
+export default function Error() {
+    return (
+        <div>
+            Eroor Page
+        </div>
+    )
+}
+
+
+const Wrapper = 
