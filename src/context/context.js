8b3491d2ac583b61c6f27e9a49404387c148a62f
vamsi krishna <vamsikrishnaadusumalli999@gmail.com>
2020-10-29 18:54:17 +0530

    wrapping up the app component with contextprovider

---
+++ src/context/context.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react'
+import { mockUsers } from './Data/mockUsers';
+import { mockFollowers } from './Data/mockFollowers';
+import { mockRepos } from './Data/mockRepos';
+
+
+
+
+const GithubContext = React.createContext();
+
+const GitHubProvider =({children} )=>{
+    const [githubUser,setGithubUser] = useState(mockUsers)
+    const[repos,setRepos] = useState[mockRepos]
+    const[followers,setFollowers] = useState[mockFollowers]
+    return(
+        <GithubContext.Provider value="hello">
+            {children}
+        </GithubContext.Provider>
+    )
+    
+}
+
+
+export{GitHubProvider,GithubContext}
