d310b421a9379f950c8090fd60a0bffa4ec2f805
vamsi krishna <vamsikrishnaadusumalli999@gmail.com>
2020-10-29 15:22:02 +0530

    basic setup and routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route,Switch} from 'react-router-dom'
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+import Error from './pages/Error';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router> 
+      <Switch>
+      <Route exact path="/"   component={Dashboard} />
+      <Route  exact  path="/login" component={Login} />
+      <Route path="*"  component={Error}/>
+      </Switch>
+    </Router>
   );
 }
 
