95b5702d7324a0a08a345ed0bfc615360b1c7097
vamsi krishna <vamsikrishnaadusumalli999@gmail.com>
2020-10-30 18:27:30 +0530

    authentication done via Auth0

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,27 @@
 import React from 'react'
 import styled from  'styled-components'
+import{useAuth0} from '@auth0/auth0-react'
+
 
 export default function Navbar() {
+  const{isAuthenticated,loginWithRedirect,logout,isLoading,user} = useAuth0()
+  const isUser = isAuthenticated&&user;
     return (
         <Wrapper>
-            
+          {isUser && user.picture && <img src={user.picture} alt={user.name}/>}
+           {isUser&&user.name&&<h4>Welcome,<strong>{user.name.toUpperCase()}</strong></h4>}
+            {isUser ?
+             <button onClick={()=>{logout({returnTo:window.location.origin})}}>
+              Logout
+            </button>
+            :
+             <button onClick={loginWithRedirect}>
+              Login
+            </button>
+            }
+           
+             
+
         </Wrapper>
     )
 }
--- src/index.js
+++ src/index.js
@@ -4,14 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { GitHubProvider } from './context/context';
-
+import { Auth0Provider } from "@auth0/auth0-react";
 ReactDOM.render(
   <React.StrictMode>
-    <GitHubProvider>
-    <App />
-    </GitHubProvider>
+    <Auth0Provider
+      domain='github-users.us.auth0.com'
+      clientId='tUA7SGJN4uJoFLru7skureIeAfpqzaH9'
+      redirectUri={window.location.origin}>
+      <GitHubProvider>
+        <App />
+      </GitHubProvider>
+    </Auth0Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
